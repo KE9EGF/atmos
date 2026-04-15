66aa951508bb05c4e18b3c27362992e1c4eddd2e
“KE9EGF” <severett078@gmail.com>
2026-04-15 15:12:42 +0000

    Updated

--- src/.ipynb_checkpoints/test-checkpoint.py
+++ src/.ipynb_checkpoints/test-checkpoint.py
@@ -2,8 +2,8 @@ import xarray as xa
 
 fname = "gfs.t06z.sfcanl.nc"
 try:
-    output = xa.open_dataset(fname, engine='netCDF4')
+    ds = xa.open_dataset(fname, engine='netCDF4')
 except Exception as e:
     print(e)
     raise e
-print(output)
+print(ds)
