bf2022eb64edc6c583f557e247dd58636460f55d
“KE9EGF” <severett078@gmail.com>
2026-04-22 20:22:43 +0000

    Updated

--- src/test.py
+++ src/test.py
@@ -1,7 +1,18 @@
-import json
+from textual.app import App, ComposeResult
+from textual.widgets import Rule, Static
 
-with open("modelConfig.json", "r") as file:
-    modelConfig = json.load(file)
+class RuleApp(App):
+    CSS = """
+    Rule {
+        margin: 1 0;
+    }
+    """
 
+    def compose(self) -> ComposeResult:
+        yield Static("Section One")
+        yield Rule(line_style="thick") # Add horizontal rule
+        yield Static("Section Two")
 
-print(modelConfig["GFS"]["baseUrl"])
+if __name__ == "__main__":
+    app = RuleApp()
+    app.run()
--- src/tui.py
+++ src/tui.py
@@ -4,10 +4,11 @@ import datetime as dt
 from random import choice
 from tqdm import tqdm
 from time import sleep
+from textual import on
+from rich import print
 from textual.app import App, ComposeResult
 from textual.widgets import Button, Header, Footer, Rule, Static
 from textual.containers import Horizontal, Vertical, VerticalScroll
-from textual import on
 
 splashTexts = [
     "NOT working since 1970!",
@@ -31,7 +32,7 @@ with open("modelConfig.json", "r") as file:
 class ModelButtons(Vertical):
     def compose(self) -> ComposeResult:
         for model in availableModels:
-            yield Button(model, id=model, classes="model-buttons")
+            yield Button(model, id=model, classes="models")
 
 class TypeButtons(Vertical):
     pass
@@ -39,7 +40,8 @@ class TypeButtons(Vertical):
 class MyApp(App):
     CSS_PATH = "tui.tcss"
     def compose(self) -> ComposeResult:
-        yield VerticalScroll(ModelButtons())
+        with Vertical:
+            yield VerticalScroll(Static("[i]Models[i]"), ModelButtons())
         yield Header()
         yield Footer()
 
